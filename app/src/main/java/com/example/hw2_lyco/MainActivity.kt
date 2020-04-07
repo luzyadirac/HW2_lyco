b51ed4b92c880596745a069853519bae6317d5fc
luzyadirac <luzyadirac@gmail.com>
2020-04-07 00:29:27 -0500

    Se agregan dos listas una fija de actividades sugeridas y la dinamica que recibe de la

--- app/src/main/java/com/example/hw2_lyco/MainActivity.kt
+++ app/src/main/java/com/example/hw2_lyco/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.hw2_lyco
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +14,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Lista_diara.setOnItemClickListener { parent, view, position, id ->  }
+        val listado = arrayListOf<String>()
+            listado.add("uno")
+            listado.add("dos")
+            listado.add("tres")
 
+        val adaptador = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listado)
+        listaAgregar.adapter = adaptador
+
+        /*listaAgregar.setOnItemLongClickListener { parent, view, position, id ->
+            val textico = view as TextView
+            listado.remove(textico.text.toString())
+            Toast.makeText(this, view.text.toString(), Toast.LENGTH_SHORT).show()
+            adaptador.notifyDataSetChanged()
+            listaAgregar.adapter = adaptador
+        }   */
+        Lista_diara.setOnItemClickListener { Lista, dato, position, id ->
+            val textView = dato as TextView
+            Toast.makeText(this, dato.text.toString(), Toast.LENGTH_SHORT).show()
+            listado.add(dato.text.toString())
+            adaptador.notifyDataSetChanged()
+            }
     }
 }
