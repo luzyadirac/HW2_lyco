9bda2580dc3547dec0378cf47d685df5f3dda53f
luzyadirac <luzyadirac@gmail.com>
2020-04-11 19:30:54 -0500

    Se agregan dos listas una fija de actividades sugeridas y la dinamica que recibe de la Se hacen mejoras...sólo falta ajustar de del archivo plano

--- app/src/main/java/com/example/hw2_lyco/MainActivity.kt
+++ app/src/main/java/com/example/hw2_lyco/MainActivity.kt
@@ -2,38 +2,50 @@ package com.example.hw2_lyco
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    //val listado = mutableListOf<string>()
+   val listado = arrayListOf<String>()
+    var band = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val listado = arrayListOf<String>()
-            listado.add("uno")
-            listado.add("dos")
-            listado.add("tres")
-
-        val adaptador = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listado)
-        listaAgregar.adapter = adaptador
-
-        /*listaAgregar.setOnItemLongClickListener { parent, view, position, id ->
-            val textico = view as TextView
-            listado.remove(textico.text.toString())
-            Toast.makeText(this, view.text.toString(), Toast.LENGTH_SHORT).show()
-            adaptador.notifyDataSetChanged()
-            listaAgregar.adapter = adaptador
-        }   */
         Lista_diara.setOnItemClickListener { Lista, dato, position, id ->
             val textView = dato as TextView
             Toast.makeText(this, dato.text.toString(), Toast.LENGTH_SHORT).show()
-            listado.add(dato.text.toString())
-            adaptador.notifyDataSetChanged()
+            actualiza_lista(dato.text.toString())
+            band = 0
             }
+
+        listaAgregar.setOnItemClickListener { Lista, dato, position, id ->
+            val textView = dato as TextView
+            band = 1
+            actualiza_lista(dato.text.toString())
+        }
+    }
+    fun actualiza_lista(m:String){
+        val adaptador = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listado)
+        listaAgregar.adapter = adaptador
+        if (band == 0) {
+            listado.add(m)
+        }
+        if (band == 1) {
+            listado.remove(m)
+            band=0
+        }
+        adaptador.notifyDataSetChanged()
+    }
+
+    fun agregarItem(este : View){
+        val nuevo = findViewById(R.id.editText) as EditText
+        actualiza_lista(nuevo.text.toString())
+        nuevo.setText("")
     }
 }
