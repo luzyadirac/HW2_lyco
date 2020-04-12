0b10aa22315a401a2500da971a204c0fe5ee0787
luzyadirac <luzyadirac@gmail.com>
2020-04-11 23:43:34 -0500

    TEngo do preguntas, 1-Cómo borrar del archivo lo que se borre de la lista? y ¿cómo hago para que muestre la lista desde el inicio con lo que carga del .tx? y la ñapa..si el archivo no existe???

--- app/src/main/java/com/example/hw2_lyco/MainActivity.kt
+++ app/src/main/java/com/example/hw2_lyco/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.hw2_lyco
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,6 +9,8 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.PrintStream
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,10 +20,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Larchivo()
+
         Lista_diara.setOnItemClickListener { Lista, dato, position, id ->
             val textView = dato as TextView
             Toast.makeText(this, dato.text.toString(), Toast.LENGTH_SHORT).show()
             actualiza_lista(dato.text.toString())
+
             band = 0
             }
 
@@ -37,13 +43,31 @@ class MainActivity : AppCompatActivity() {
             val textView = dato as TextView
             band = 1
             actualiza_lista(dato.text.toString())
+            crea_archivo(dato.text.toString())
         }
+    }
+
+    fun Larchivo(){
+        val entrada = Scanner ( openFileInput("tareas.txt"))
+
+        while(entrada.hasNextLine()) {
+            val linea = entrada.nextLine()
+            listado.addAll(linea.split(","))
+        }
+        entrada.close()
+    }
+
+    fun crea_archivo(opcion :String){
+        val salida = PrintStream (openFileOutput("tareas.txt", Context.MODE_APPEND))
+        salida.println("$opcion")
+        salida.close()
     }
     fun actualiza_lista(m:String){
         val adaptador = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listado)
         listaAgregar.adapter = adaptador
         if (band == 0) {
             listado.add(m)
+            crea_archivo(m)
         }
         if (band == 1) {
             listado.remove(m)
@@ -54,7 +78,12 @@ class MainActivity : AppCompatActivity() {
 
     fun agregarItem(este : View){
         val nuevo = findViewById(R.id.editText) as EditText
-        actualiza_lista(nuevo.text.toString())
-        nuevo.setText("")
+        if(nuevo.text.toString() == ""){
+            Toast.makeText(this, "No hay información para agregar", Toast.LENGTH_SHORT).show()
+        }
+        else {
+            actualiza_lista(nuevo.text.toString())
+            nuevo.setText("")
+        }
     }
 }
